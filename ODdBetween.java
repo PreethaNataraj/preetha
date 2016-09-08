4f298313ea9e0e3e1d749c04cf420fde3fabf974
PreethaNataraj <preethasnpk@gmail.com>
2016-09-08 16:00:46 +0530

    Add files via upload

---
+++ ODdBetween.java
@@ -0,0 +1,19 @@
+package preetha;
+
+import java.util.Scanner;
+
+public class ODdBetween {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		for(int i=0;i<=a;i++)
+		if(i%2!=0)
+		{
+			System.out.println(i);
+		}
+		
+
+	}
+
+}
