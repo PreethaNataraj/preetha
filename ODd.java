9746e1cba0fb22334c843146b463179cdc8308d4
PreethaNataraj <preethasnpk@gmail.com>
2016-09-08 12:14:08 +0530

    Add files via upload

---
+++ ODd.java
@@ -0,0 +1,21 @@
+package preetha;
+
+import java.util.Scanner;
+
+public class ODd {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		if(a%2==0)
+		{
+			System.out.println("even");
+		}
+		else
+		{
+			System.out.println("odd");
+		}
+
+	}
+
+}
