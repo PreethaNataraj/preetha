b4c6100f084c10cba6e3172738c9985251bc13c2
PreethaNataraj <preethasnpk@gmail.com>
2016-09-08 12:20:27 +0530

    Add files via upload

---
+++ Nagative.java
@@ -0,0 +1,27 @@
+package preetha;
+
+import java.util.Scanner;
+
+public class Nagative {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		if(a>0)
+		{
+			System.out.println("positive");
+		}
+		else
+		{
+			
+			System.out.println("nagative");
+			if(a==0)
+			{
+				System.out.println("the value is zero");
+			}
+		}
+		
+
+	}
+
+}
